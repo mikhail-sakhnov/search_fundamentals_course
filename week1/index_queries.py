0853761c18110e94b4c80a6aef845a20a61feb30
Mikhail Sakhnov <mikhail.sakhnov@wolt.com>
2023-10-01 22:34:04 +0300

    week1: indexing and searching

--- week1/index_queries.py
+++ week1/index_queries.py
@@ -9,15 +9,15 @@ import time
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logging.basicConfig(format='%(levelname)s:%(message)s')
+logging.basicConfig(format="%(levelname)s:%(message)s")
+
 
 def get_opensearch():
-
-    host = 'localhost'
+    host = "localhost"
     port = 9200
-    auth = ('admin', 'admin')
+    auth = ("admin", "admin")
     client = OpenSearch(
-        hosts=[{'host': host, 'port': port}],
+        hosts=[{"host": host, "port": port}],
         http_compress=True,  # enables gzip compression for request bodies
         http_auth=auth,
         # client_cert = client_cert_path,
@@ -26,35 +26,46 @@ def get_opensearch():
         verify_certs=False,
         ssl_assert_hostname=False,
         ssl_show_warn=False,
-        #ca_certs=ca_certs_path
+        # ca_certs=ca_certs_path
     )
     return client
 
+
 @click.command()
-@click.option('--source_file', '-s', help='source csv file', required=True)
-@click.option('--index_name', '-i', default="bbuy_queries", help="The name of the index to write to")
+@click.option("--source_file", "-s", help="source csv file", required=True)
+@click.option(
+    "--index_name",
+    "-i",
+    default="bbuy_queries",
+    help="The name of the index to write to",
+)
 def main(source_file: str, index_name: str):
     client = get_opensearch()
     ds = pd.read_csv(source_file)
-    #print(ds.columns)
-    ds['click_time'] = pd.to_datetime(ds['click_time'])
-    ds['query_time'] = pd.to_datetime(ds['query_time'])
-    #print(ds.dtypes)
+    # print(ds.columns)
+    ds["click_time"] = pd.to_datetime(ds["click_time"], format="ISO8601")
+    ds["query_time"] = pd.to_datetime(ds["query_time"], format="ISO8601")
+    # print(ds.dtypes)
     docs = []
     tic = time.perf_counter()
     for idx, row in ds.iterrows():
         doc = {}
         for col in ds.columns:
+            if pd.isna(row[col]):
+                continue
             doc[col] = row[col]
-        docs.append({'_index': index_name , '_source': doc})
+        docs.append({"_index": index_name, "_source": doc})
         if idx % 1000 == 0:
             bulk(client, docs, request_timeout=60)
-            logger.info(f'{idx} documents indexed')
+            logger.info(f"{idx} documents indexed")
             docs = []
     if len(docs) > 0:
         bulk(client, docs, request_timeout=60)
     toc = time.perf_counter()
-    logger.info(f'Done indexing {ds.shape[0]} records. Total time: {((toc-tic)/60):0.3f} mins.')
+    logger.info(
+        f"Done indexing {ds.shape[0]} records. Total time: {((toc-tic)/60):0.3f} mins."
+    )
+
 
 if __name__ == "__main__":
     main()
--- week1/search.py
+++ week1/search.py
@@ -1,13 +1,11 @@
 #
 # The main search hooks for the Search Flask application.
 #
-from flask import (
-    Blueprint, redirect, render_template, request, url_for
-)
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from week1.opensearch import get_opensearch
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+bp = Blueprint("search", __name__, url_prefix="/search")
 
 
 # Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display of the applied filters
@@ -17,13 +15,16 @@ bp = Blueprint('search', __name__, url_prefix='/search')
 def process_filters(filters_input):
     # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
-    display_filters = []  # Also create the text we will use to display the filters that are applied
+    display_filters = (
+        []
+    )  # Also create the text we will use to display the filters that are applied
     applied_filters = ""
     for filter in filters_input:
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName", filter)
-        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
-                                                                                 display_name)
+        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(
+            filter, filter, type, filter, display_name
+        )
         if type == "range":
             from_val = request.args.get(filter + ".from", None)
             to_val = request.args.get(filter + ".to", None)
@@ -40,26 +41,30 @@ def process_filters(filters_input):
                 to_val = "*"  # set it to * for display purposes, but don't use it in the query
             the_filter = {"range": {filter: to_from}}
             filters.append(the_filter)
-            display_filters.append("{}: {} TO {}".format(display_name, from_val, to_val))
-            applied_filters += "&{}.from={}&{}.to={}".format(filter, from_val, filter, to_val)
+            display_filters.append(
+                "{}: {} TO {}".format(display_name, from_val, to_val)
+            )
+            applied_filters += "&{}.from={}&{}.to={}".format(
+                filter, from_val, filter, to_val
+            )
         elif type == "terms":
             field = request.args.get(filter + ".fieldName", filter)
             key = request.args.get(filter + ".key", None)
             the_filter = {"term": {field: key}}
             filters.append(the_filter)
             display_filters.append("{}: {}".format(display_name, key))
-            applied_filters += "&{}.fieldName={}&{}.key={}".format(filter, field, filter, key)
+            applied_filters += "&{}.fieldName={}&{}.key={}".format(
+                filter, field, filter, key
+            )
     print("Filters: {}".format(filters))
 
     return filters, display_filters, applied_filters
 
 
-
 # Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
-@bp.route('/query', methods=['GET', 'POST'])
+@bp.route("/query", methods=["GET", "POST"])
 def query():
-    opensearch = get_opensearch() # Load up our OpenSearch client from the opensearch.py file.
-    # Put in your code to query opensearch.  Set error as appropriate.
+    opensearch = get_opensearch()
     error = None
     user_query = None
     query_obj = None
@@ -68,8 +73,8 @@ def query():
     filters = None
     sort = "_score"
     sortDir = "desc"
-    if request.method == 'POST':  # a query has been submitted
-        user_query = request.form['query']
+    if request.method == "POST":  # a query has been submitted
+        user_query = request.form["query"]
         if not user_query:
             user_query = "*"
         sort = request.form["sort"]
@@ -79,7 +84,9 @@ def query():
         if not sortDir:
             sortDir = "desc"
         query_obj = create_query(user_query, [], sort, sortDir)
-    elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
+    elif (
+        request.method == "GET"
+    ):  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
         filters_input = request.args.getlist("filter.name")
         sort = request.args.get("sort", sort)
@@ -92,16 +99,24 @@ def query():
         query_obj = create_query("*", [], sort, sortDir)
 
     print("query obj: {}".format(query_obj))
+    try:
+        response = opensearch.search(
+            body=query_obj,
+            index="bbuy_products",
+        )
+    except Exception as e:
+        error = e
 
-    #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
-    # Postprocess results here if you so desire
-
-    #print(response)
     if error is None:
-        return render_template("search_results.jinja2", query=user_query, search_response=response,
-                               display_filters=display_filters, applied_filters=applied_filters,
-                               sort=sort, sortDir=sortDir)
+        return render_template(
+            "search_results.jinja2",
+            query=user_query,
+            search_response=response,
+            display_filters=display_filters,
+            applied_filters=applied_filters,
+            sort=sort,
+            sortDir=sortDir,
+        )
     else:
         redirect(url_for("index"))
 
@@ -109,13 +124,53 @@ def query():
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
     print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
     query_obj = {
-        'size': 10,
+        "size": 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "bool": {
+                "must": [
+                    {
+                        "query_string": {
+                            "fields": [
+                                "name^100",
+                                "shortDescription^50",
+                                "longDescription^10",
+                            ],
+                            "query": user_query,
+                            "phrase_slop": 3,
+                        }
+                    }
+                ],
+                "filter": filters,
+            }
         },
         "aggs": {
-            #### Step 4.b.i: create the appropriate query and aggregations here
-
-        }
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",  # Field to perform the range aggregation on
+                    "ranges": [
+                        {"from": 0, "to": 10},
+                        {"from": 10, "to": 20},
+                        {"from": 20, "to": 30},
+                        {"from": 30, "to": 40},
+                        {"from": 40, "to": 50},
+                        {"from": 50, "to": 100},
+                        {"from": 100},
+                    ],
+                }
+            },
+            "department": {"terms": {"field": "department.keyword", "size": 10}},
+            "missing_images": {"missing": {"field": "image"}},
+        },
+        "sort": [
+            {"regularPrice": {"order": "asc"}},
+            {"name.keyword": {"order": "asc"}},
+        ],
+        "highlight": {
+            "fields": {
+                "name": {},  # Highlighting for the "name" field
+                "shortDescription": {},  # Highlighting for the "shortDescription" field
+                "longDescription": {},  # Highlighting for the "longDescription" field
+            }
+        },
     }
     return query_obj
